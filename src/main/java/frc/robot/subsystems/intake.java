ee13b1d7aaba05bce65fc90d5b5a305b12d23eae
superxman1 <nickwickler1@gmail.com>
2022-02-05 11:55:08 -0500

    initial

---
+++ src/main/java/frc/robot/subsystems/intake.java
@@ -0,0 +1,39 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class intake extends SubsystemBase {
+  /** Creates a new motor. */
+  CANSparkMax _m;
+  DigitalInput _button;
+  private static intake _instance = new intake();
+  public intake() {
+    _m = new CANSparkMax(3, MotorType.kBrushless);
+    _button = new DigitalInput(0);
+  }
+  public void set(double vbus){
+    _m.set(vbus);
+  }
+  public boolean isClosed(){
+    return _button.get();
+  }
+   public static intake get_instance() {
+      return _instance;
+   }
+    
+
+
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
